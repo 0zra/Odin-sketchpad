9795fc1865e21bd38f245f33fe3873f8b021da65
ozra <petar.ozretic@mail.com>
2017-10-20 09:33:52 +0200

    Add buttons, make more functional with functions + fix bugs

--- aplication.js
+++ aplication.js
@@ -1,13 +1,29 @@
+let box = $('<div class="square"></div>');
+$.fn.formiranje = function(dim) {
+  $(this).empty();
+  for(let i = 1; i <= dim*dim; i++){
+  $(this).prepend(box.clone());}
+};
 $(document).ready(function() {
-  let box = $('<div class="square"></div>');
 
+// pocetne postavke
+  let basic = true;
+  let random = false;
+  let gray = false;
+// formiranje tablice prvi put
+  $('#conteiner').formiranje(16);
 
-    $('#conteiner').prepend(box);
-    for(let i = 1; i < 256; i++){
-    $('#conteiner').prepend(box.clone());
-    }
+//nova tablica
+  $('#clear').on('click', function() {
+      $('#conteiner').formiranje(16);
+  });
 
-
+// provjera koja je opcija ukljucena
+  if( basic === true) {
+    $('.square').on('mouseenter', function() {
+    $(this).addClass('visited');
+    });
+  };
 
 
 
