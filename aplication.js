d350456373fb7060b83c9f0a2cd8110171322e48
ozra <petar.ozretic@mail.com>
2017-10-21 15:04:34 +0200

    Make 50 shades of gray option available

--- aplication.js
+++ aplication.js
@@ -2,22 +2,18 @@
 
 
 $(document).ready(function() {
-  // general settings
-    let basic = true;
-    let random = false;
-    let gray = false;
   //forming function
   $.fn.formiranje = function(dim) {
     basic = true;
-    let box = $('<div class="square nova boja"></div>');
+    let box = $('<div class="square nova boja" data-c="256"></div>');
     $(this).empty();
     for(let i = 1; i <= dim*dim; i++){
     $(this).prepend(box.clone());}
   //basic function
-  if(basic === true){
+
     $('.square').on('mouseenter', function() {
     $(this).css("background-color","black");
-  });};
+  });
 
   };
 
@@ -32,13 +28,10 @@ $(document).ready(function() {
     let tekst = "<style type='text/css'> .nova{ width:"+dimenzija_kutije+" !important; height:"+dimenzija_kutije+" !important;} </style>";
     $(tekst).appendTo("head");
     $('#conteiner').formiranje(Number(dimenzija));
-
-
-
   });
+//random option
   $('#random').on('click', function() {
-    basic = false;
-    random = true;
+
     $('.square').on('mouseenter', function() {
       let r = Math.floor(Math.random()*256);
       let g = Math.floor(Math.random()*256);
@@ -46,8 +39,21 @@ $(document).ready(function() {
       let boja ='rgb('+r+','+g+','+b+')';
       $(this).css('background-color',boja);
 
+      });
   });
+// "50" shades of gray option
+  $('#gray').on('click', function() {
 
+    $('.square').on('mouseenter', function() {
+        let c = +$(this).data('c')
+        c=c-20;
+        let zapis = ''+c+'';
+        $(this).data('c', zapis);
+        let b = 'rgb('+c+','+c+','+c+')';
+        $(this).css('background-color',b);
+
+
+      });
 
   });
 
